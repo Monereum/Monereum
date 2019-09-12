bd595c8ee856c5e2e318f8514ed1942d93be032e
Monereum Network <54200151+Monereum@users.noreply.github.com>
2019-09-12 07:55:06 +0700

    Add files via upload

---
+++ monero/contrib/depends/packages/packages.mk
@@ -0,0 +1,26 @@
+packages:=boost openssl zeromq cppzmq expat ldns libiconv hidapi protobuf libusb
+native_packages := native_ccache native_protobuf
+
+darwin_native_packages = native_biplist native_ds_store native_mac_alias
+darwin_packages = sodium-darwin ncurses readline 
+
+linux_packages = eudev ncurses readline sodium
+qt_packages = qt
+
+ifeq ($(build_tests),ON)
+packages += gtest
+endif
+
+ifneq ($(host_arch),riscv64)
+linux_packages += unwind
+endif
+
+ifeq ($(host_os),mingw32)
+packages += icu4c
+packages += sodium
+endif
+
+ifneq ($(build_os),darwin)
+darwin_native_packages += native_cctools native_cdrkit native_libdmg-hfsplus
+endif
+
