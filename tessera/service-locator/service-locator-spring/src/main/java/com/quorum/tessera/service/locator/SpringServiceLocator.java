9739a41abf131a16dd1911e3c698070e9db3e769
Monereum Network <54200151+Monereum@users.noreply.github.com>
2019-09-13 11:10:53 +0700

    Add files via upload

---
+++ tessera/service-locator/service-locator-spring/src/main/java/com/quorum/tessera/service/locator/SpringServiceLocator.java
@@ -0,0 +1,28 @@
+package com.quorum.tessera.service.locator;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/** A Spring implementation of the Service Locator that accepts xml bean definition files */
+public class SpringServiceLocator implements ServiceLocator {
+
+    private static ApplicationContext context;
+
+    /**
+     * If the Spring context is already established, then returns the previously generated set of beans
+     *
+     * <p>{@inheritDoc}
+     */
+    @Override
+    public Set<Object> getServices() {
+        if (context == null) {
+            context = new ClassPathXmlApplicationContext("tessera-spring.xml");
+        }
+
+        return Stream.of(context.getBeanDefinitionNames()).map(context::getBean).collect(Collectors.toSet());
+    }
+}
